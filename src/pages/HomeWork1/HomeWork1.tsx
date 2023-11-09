80867e2650e126f3d26799d4e2cb4754dfe8c89c
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-11-09 11:57:26 +0300

    hw1-complete

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+
+  const [result, setResult] = useState<number>()
+
+  const randomize = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  } 
+
+  const rollDice1_6 = () => setResult(randomize(1, 6)) 
+
+  return <div>
+    <h2>Симулятор игральной кости</h2>
+    <div>
+      <span>
+        <p>Кость 1-6</p>
+        <button onClick={rollDice1_6}>Бросок</button>
+        <p>Результат: `${result}`</p>
+      </span>
+    </div>
+  </div>;
 };
