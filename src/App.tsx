cb9029782bea2d9c8c478e1fb3302be3cad129e3
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-11-22 11:31:34 +0300

    solution to hw-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { routes, routesConfig } from "./routes";
 import { Layout } from "./components/Layout/Layout";
+import { HomeWork2 } from "./pages/HomeWork2/HomeWork2";
 
 function App() {
   return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          {routes.slice(1).map((route) => (
-            <Route
-              key={route}
-              path={route}
-              element={routesConfig[route].element}
-            />
-          ))}
-          {routes.length && (
-            <Route path={"*"} element={routesConfig[routes[0]].element} />
-          )}
-        </Routes>
-      </Layout>
-    </HashRouter>
+    <HomeWork2/>
+    // <HashRouter>
+    //   <Layout>
+    //     <Routes>
+    //       {routes.slice(1).map((route) => (
+    //         <Route
+    //           key={route}
+    //           path={route}
+    //           element={routesConfig[route].element}
+    //         />
+    //       ))}
+    //       {routes.length && (
+    //         <Route path={"*"} element={routesConfig[routes[0]].element} />
+    //       )}
+    //     </Routes>
+    //   </Layout>
+    // </HashRouter>
   );
 }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -14,9 +14,56 @@ export const routesConfig: Record<string, IRoute> = {
     element: <HomeWork1 />,
   },
   "/homework2": {
-    title: "Домашнее задание №2",
+    title: "Домашнее задание №2: Виджет для скролла",
     element: <HomeWork2 />,
   },
+  // "/homework3": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },
+  // "/homework4": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework5": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework6": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework7": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework8": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework9": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework10": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework11": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework12": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework13": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework14": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework15": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework16": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },"/homework17": {
+  //   title: "Домашнее задание №2: Виджет для скролла",
+  //   element: <HomeWork2 />,
+  // },
 };
 
 export const routes = Object.keys(routesConfig);
